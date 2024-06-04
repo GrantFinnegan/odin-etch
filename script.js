9df631c1f93f8a2b187b69b1a3653005a361776a
GrantFinnegan <grantfinnegan@gmail.com>
2024-06-04 12:57:53 -0500

    add section to script to populate container

---
+++ script.js
@@ -0,0 +1,21 @@
+const gridSize = 16;
+
+//get container of the squares
+const container = document.querySelector("#container");
+
+
+//FOR gridSize:
+//  add row div
+//  FOR gridSize: add square divs to row div
+
+for (let i = 0; i < gridSize; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+
+    for (let j = 0; j < gridSize; j++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+        row.appendChild(square);
+    }
+}
