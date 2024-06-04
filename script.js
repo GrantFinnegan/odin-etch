db76a6ad3664d151914670573d0934569105494b
GrantFinnegan <grantfinnegan@gmail.com>
2024-06-04 13:59:52 -0500

    add button to regenerate grid of arbitrary size

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
-const gridSize = 16;
-
-//get container of the squares
+//get container of the squares. This will hold the grid
 const container = document.querySelector("#container");
 
 
-//FOR gridSize:
-//  add row div
-//  FOR gridSize: add square divs to row div
+//populate grid container with gridSize by gridSize set of .square divs
+//grid is composed of gridSize .row divs, each containing gridSize square divs
+function createGrid(gridSize) {
+    //remove any and all preexisting .square divs
+    container.replaceChildren();
 
-for (let i = 0; i < gridSize; i++) {
-    let row = document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
+    //add gridSize rows
+    for (let i = 0; i < gridSize; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
 
-    for (let j = 0; j < gridSize; j++) {
-        let square = document.createElement("div");
-        square.classList.add("square");
-        row.appendChild(square);
-        square.addEventListener("mouseenter", () => square.classList.add("darkened"));
+        //add gridSize squares to each row
+        for (let j = 0; j < gridSize; j++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            row.appendChild(square);
+            square.addEventListener("mouseenter", () => square.classList.add("darkened"));
+        }
     }
 }
+
+//add event listener to the grid creation button
+document.querySelector("#grid-creation-btn").addEventListener("click",
+ () => createGrid(prompt("Enter grid size (Max 100)"))
+);
+
+//initialize grid on 
+createGrid(16);
